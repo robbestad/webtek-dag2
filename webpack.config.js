9615fc963f7d2c2622f76d96a011a2a77cd1364a
Sven Anders Robbestad <robbestad@gmail.com>
2018-08-17 14:50:24 +0200

    Add history rewrite for koa and webpack serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
+
 module.exports = {
 	entry:   path.resolve(__dirname, "src/index.jsx"),
 	mode:    "development",
@@ -11,7 +15,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin(
 			{
-				title: 'Bedre enn Instagram',
+				title:    'Bedre enn Instagram',
 				template: path.resolve(__dirname, "src/index.html")
 			}
 		)
@@ -35,11 +39,24 @@ module.exports = {
 			}
 		]
 	},
-	resolve:{
+	resolve: {
 		extensions: [".js", ".jsx", ".css"],
-		alias: {
-			src: path.resolve(__dirname ,"src")
+		alias:      {
+			src: path.resolve(__dirname, "src")
 		}
+	}
+}
+module.exports.serve = {
+	content: [__dirname],
+	add:     (app, middleware, options) => {
+		const historyOptions = {
+			rewrites: [
+				{from: /\/*bundle.js$/, to: '/bundle.js'}
+			],
+			index:    '/index.html',
+			logger:   console.log.bind(console)
+		};
+		app.use(convert(history(historyOptions)));
 	}
 
 }
