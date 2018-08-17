f6acd23cebaabbe95794119fc4d6dbee195a7370
Sven Anders Robbestad <robbestad@gmail.com>
2018-08-17 11:40:17 +0200

    Setup v1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-	entry:   path.resolve(__dirname, "src/index.js"),
+	entry:   path.resolve(__dirname, "src/index.jsx"),
 	mode:    "development",
 	output:  {
 		path:     path.resolve(__dirname, "dist"),
